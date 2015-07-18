91fdca7a01944cf809412dde9a8be44a3be98b9d
taoka <mrp1q7z@gmail.com>
2015-07-18 11:31:27 +0900

    管理者用のモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150717114052) do
+ActiveRecord::Schema.define(version: 20150718015007) do
+
+  create_table "administrators", force: :cascade do |t|
+    t.string   "email",                           null: false
+    t.string   "email_for_index",                 null: false
+    t.string   "hashed_password"
+    t.boolean  "suspended",       default: false, null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+  end
+
+  add_index "administrators", ["email_for_index"], name: "index_administrators_on_email_for_index", unique: true
 
   create_table "staff_members", force: :cascade do |t|
     t.string   "email",                            null: false
