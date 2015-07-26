4315a544084a840db2255a0f177837b2d0fe0c43
taoka <mrp1q7z@gmail.com>
2015-07-26 12:57:23 +0900

    Admin::StaffMembersコントローラーのdestroyアクションを実装

--- app/controllers/admin/staff_members_controller.rb
+++ app/controllers/admin/staff_members_controller.rb
@@ -36,6 +36,13 @@ class Admin::StaffMembersController < Admin::Base
     end
   end
 
+  def destroy
+    @staff_member = StaffMember.find(params[:id])
+    @staff_member.destroy!
+    flash.notice = '職員アカウントを削除しました。'
+    redirect_to :admin_staff_members
+  end
+
 private
 
   def staff_member_params
