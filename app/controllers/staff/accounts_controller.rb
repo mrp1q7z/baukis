5f2359d1233e6ca524d7f20425a33ea597032035
taoka <mrp1q7z@gmail.com>
2015-07-28 20:39:39 +0900

    職員のedit/updateアクションを実装

--- app/controllers/staff/accounts_controller.rb
+++ app/controllers/staff/accounts_controller.rb
@@ -2,4 +2,24 @@ class Staff::AccountsController < Staff::Base
   def show
     @staff_member = current_staff_member
   end
+
+  def edit
+    @staff_member = current_staff_member
+  end
+
+  def update
+    @staff_member = current_staff_member
+    if @staff_member.update(staff_member_params)
+      flash.notice = 'アカウント情報を更新しました'
+      redirect_to :staff_account
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+  
+  def staff_member_params
+    params.require(:staff_member).permit(:email, :family_name, :given_name, :family_name_kana, :given_name_kana)
+  end
 end
