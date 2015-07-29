4261922a06b76b5607d209089af04dc1dfd94dff
taoka <mrp1q7z@gmail.com>
2015-07-29 21:00:19 +0900

    Staff::セッションタイムアウトの処理を実装

--- app/controllers/staff/base.rb
+++ app/controllers/staff/base.rb
@@ -2,6 +2,7 @@ module Staff
   class Base < ApplicationController
     before_action :authorize
     before_action :check_account
+    before_action :check_timeout
 
     private
 
@@ -23,6 +24,19 @@ module Staff
         session.delete(:staff_login)
         flash.alert = 'アカウントが無効になりました。'
         redirect_to :staff_root
+      end
+    end
+
+    TIMEOUT = 60.minutes
+    def check_timeout
+      if current_staff_member
+        if session[:last_access_time] >= TIMEOUT.ago
+          session[:last_access_time] = Time.current
+        else
+          session.delete(:staff_member_id)
+          flash.alert = 'セッションがタイムアウトしました。'
+          redirect_to :staff_login
+        end
       end
     end
 
