8b001875eb4500f01d939e9322c0f1feed065eba
taoka <mrp1q7z@gmail.com>
2015-07-29 20:38:48 +0900

    Staff::ログインしてない場合はログイン画面へリダイレクトするようにした

--- app/controllers/staff/base.rb
+++ app/controllers/staff/base.rb
@@ -1,10 +1,19 @@
 module Staff
   class Base < ApplicationController
+    before_action :authorize
+
     private
 
     def current_staff_member
       if session[:staff_member_id]
         @current_staff_member ||= StaffMember.find_by(id: session[:staff_member_id])
+      end
+    end
+
+    def authorize
+      unless current_staff_member
+        flash.alert = '職員としてログインしてください。'
+        redirect_to :staff_login
       end
     end
 
