4ebc32a9d26897d19b05c071f16a48100971f388
taoka <mrp1q7z@gmail.com>
2015-08-03 21:38:09 +0900

    Admin::職員のログイン・ログアウト履歴を表示する機能を追加

--- app/models/staff_event.rb
+++ app/models/staff_event.rb
@@ -3,4 +3,15 @@ class StaffEvent < ActiveRecord::Base
 
   belongs_to :member, class_name: 'StaffMember', foreign_key: 'staff_member_id'
   alias_attribute :occurred_at, :created_at
+
+  DESCRIPTIONS = {
+      logged_in: 'ログイン',
+      logged_out: 'ログアウト',
+      rejected: 'ログイン拒否',
+      password_error: 'パスワードエラー'
+  }
+
+  def description
+    DESCRIPTIONS[type.to_sym]
+  end
 end
