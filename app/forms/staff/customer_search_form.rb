1bcd47f7f8c971ec56eb5a658962ad9f7d3520cb
taoka <mrp1q7z@gmail.com>
2015-09-13 08:57:56 +0900

    顧客を郵便番号で検索できるようにした

--- app/forms/staff/customer_search_form.rb
+++ app/forms/staff/customer_search_form.rb
@@ -5,7 +5,7 @@ class Staff::CustomerSearchForm
   attr_accessor :family_name_kana, :given_name_kana,
       :birth_year, :birth_month, :birth_mday,
       :address_type, :prefecture, :city, :phone_number,
-      :gender
+      :gender, :postal_code
 
   def search
     normalize_values
@@ -22,7 +22,7 @@ class Staff::CustomerSearchForm
     rel = rel.where(birth_mday: birth_mday) if birth_mday.present?
     rel = rel.where(gender: gender) if gender.present?
 
-    if prefecture.present? || city.present?
+    if prefecture.present? || city.present? || postal_code.present?
       case address_type
         when  'home'
           rel = rel.joins(:home_address)
@@ -37,6 +37,7 @@ class Staff::CustomerSearchForm
         rel = rel.where('addresses.prefecture' => prefecture)
       end
       rel = rel.where('addresses.city' => city) if city.present?
+      rel = rel.where('addresses.postal_code' => postal_code) if postal_code.present?
     end
 
     if phone_number.present?
@@ -53,5 +54,6 @@ class Staff::CustomerSearchForm
     self.given_name_kana = normalize_as_furigana(given_name_kana)
     self.city = normalize_as_name(city)
     self.phone_number = normalize_as_phone_number(phone_number).try(:gsub, /\D/, '')
+    self.postal_code = normalize_as_postal_code(postal_code).try(:gsub, /\D/, '')
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150912233234) do
+ActiveRecord::Schema.define(version: 20150912234301) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer  "customer_id",                null: false
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20150912233234) do
 
   add_index "addresses", ["city"], name: "index_addresses_on_city"
   add_index "addresses", ["customer_id"], name: "index_addresses_on_customer_id"
+  add_index "addresses", ["postal_code"], name: "index_addresses_on_postal_code"
   add_index "addresses", ["prefecture", "city"], name: "index_addresses_on_prefecture_and_city"
   add_index "addresses", ["type", "city"], name: "index_addresses_on_type_and_city"
   add_index "addresses", ["type", "customer_id"], name: "index_addresses_on_type_and_customer_id", unique: true
